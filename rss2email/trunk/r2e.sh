dd910340e5ad4b39c8c781cde63c16caf36ed6cb
Pierre Schmitz <pierre@archlinux.de>
2015-10-03 07:56:32 +0000

    upgpkg: rss2email 2.71-4
    git-svn-id: file:///home/pierre/svn@879 58d9c5f6-d2ea-4a71-8962-830f3da2cfcc

--- rss2email/trunk/r2e.sh
+++ rss2email/trunk/r2e.sh
@@ -1,4 +1,5 @@
-#!/bin/bash
-mkdir -p $HOME/.rss2email
-cd $HOME/.rss2email
-python2 /usr/share/rss2email/bin/rss2email.py feeds.dat "$@"
+#!/bin/sh
+
+mkdir -p ${HOME}/.rss2email
+cd ${HOME}/.rss2email
+python2 /usr/lib/rss2email/rss2email.py feeds.dat "$@"
