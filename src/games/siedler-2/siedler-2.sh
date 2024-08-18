ffe4bfab9ce6bb1bbee5e5899dfbfde82315cf11
Pierre Schmitz <pierre@archlinux.de>
2024-08-18 15:00:07 +0200

    Add Siedler 2 editor

--- src/games/siedler-2/siedler-2.sh
+++ src/games/siedler-2/siedler-2.sh
@@ -1,3 +1,3 @@
 #!/usr/bin/env sh
 mkdir -p $HOME/.config/siedler-2
-dosbox --working-dir /usr/share/siedler-2 --noprimaryconf --conf dosbox.conf
+dosbox --working-dir /usr/share/siedler-2 --noprimaryconf --conf dosbox-s2.conf
