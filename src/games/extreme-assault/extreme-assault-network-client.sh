1956fe2d5cec964b20f588c71a9b7506ba7ea35e
Pierre Schmitz <pierre@archlinux.de>
2024-08-31 15:22:17 +0200

    Add network support to Extreme Assault

---
+++ src/games/extreme-assault/extreme-assault-network-client.sh
@@ -0,0 +1,3 @@
+#!/usr/bin/env sh
+mkdir -p $HOME/.config/extreme-assault
+dosbox --working-dir /usr/share/extreme-assault --noprimaryconf --conf dosbox-network-client.conf
--- src/games/extreme-assault/extreme-assault.sh
+++ src/games/extreme-assault/extreme-assault.sh
@@ -1,3 +1,3 @@
 #!/usr/bin/env sh
 mkdir -p $HOME/.config/extreme-assault
-dosbox --working-dir /usr/share/extreme-assault --noprimaryconf --conf dosbox.conf
+dosbox --working-dir /usr/share/extreme-assault --noprimaryconf --conf dosbox-start.conf
